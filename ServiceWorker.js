b0259bde1e8c3d653836b4e3c64d1eaf928bc707
efimmaksimov <efimmaksimov6106@gmail.com>
2024-05-16 21:26:38 +0500

    first commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Photon Engine-Quiz Network-2.0.0.13";
+const contentToCache = [
+    "Build/Photon test.loader.js",
+    "Build/Photon test.framework.js.br",
+    "Build/Photon test.data.br",
+    "Build/Photon test.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
